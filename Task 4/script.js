30503eecec33a4ee9a268be74bf3050d34436211
KarolisZemelis <karoliszemelis@gmail.com>
2022-06-23 18:09:54 +0300

    TASK4_WORKING_FETCH_WITH_LOOP

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,27 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+async function getUsers() {
+  const response = await fetch(ENDPOINT);
+  if (!response.ok) {
+    throw new Error("Įvyko klaida: " + response.status);
+  } else {
+    const result = await response.json();
+
+    const resultArray =(result.map((car)=>car.models))
+    // const resultArray = Object.entries(result);
+    // console.log(resultArray.map((car)=>car.models))
+    for (let i = 0; i < result.length; i++) {
+        console.log(result[i].brand)
+        console.log(resultArray[i])
+
+    }
+
+
+  }
+}
+
+
+
+getUsers();
