14306847500c4c7edf4f974bd484739ba030d246
KarolisZemelis <karoliszemelis@gmail.com>
2022-06-23 18:21:20 +0300

    FIRST_WORKING_VERSION

--- Task 4/script.js
+++ Task 4/script.js
@@ -18,18 +18,36 @@ async function getUsers() {
     const result = await response.json();
 
     const resultArray =(result.map((car)=>car.models))
+    const mainContainer = document.getElementById('output')
+
     // const resultArray = Object.entries(result);
     // console.log(resultArray.map((car)=>car.models))
     for (let i = 0; i < result.length; i++) {
         console.log(result[i].brand)
         console.log(resultArray[i])
-
+        const brandContainer = document.createElement('div');
+        brandContainer.setAttribute('class','brandContainer');
+        brandContainer.innerHTML = `
+        <h1>${result[i].brand}</h1>
+        <h2>${resultArray[i]}</h2>
+        `;
+        main.append(brandContainer);
     }
 
 
   }
 }
 
-
+// function output(brand,models,mainContainer,brandContainer){
+//     console.log('funkcija prasideda')
+//     // const mainContainer = document.getElementById('#output')
+//     // const brandContainer = document.createElement('div');
+//     // brandContainer.setAttribute('class','brandContainer');
+//     brandContainer.innerHTML = `
+//     <h1>${brand}</h1>
+//     <h2>${models}</h2>
+//     `;
+//     mainContainer.append(brandContainer);
+// }
 
 getUsers();
