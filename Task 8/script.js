46658ab905eb226a5625c429abfee03825c17633
KarolisZemelis <karoliszemelis@gmail.com>
2022-06-24 08:52:59 +0300

    OPERATIONAL_CALCULATOR_FINISHED

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,28 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+'use strict'
+
+function Calculator(a,b){
+    this.numberA = a;
+    this.numberB = b;
+    this.sum = function(){
+        return this.numberA + this.numberB
+    }
+    this.subtraction = function(){
+        return this.numberA -this.numberB
+    }
+    this.multiplication = function(){
+        return this.numberA * this.numberB
+    }
+    this.division = function(){
+        return this.numberA / this.numberB
+    }
+}
+
+
+var Calculation = new Calculator(10,5);
+console.log(`Calculator sum method of ${Calculation.sum()}`);
+console.log(`Calculator subtraction method ${Calculation.subtraction()}`);
+console.log(`Calculator multiplication method ${Calculation.multiplication()}`);
+console.log(`Calculator division method ${Calculation.division()}`);
